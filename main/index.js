c3ce3ca2d4fd9cac5fb20463f8d50dec2d178191
Arpit26garg04 <arpitgarg2604@gmail.com>
2024-04-03 16:13:25 +0530

    done

--- main/index.js
+++ main/index.js
@@ -1,4 +1,33 @@
-const scroll = new LocomotiveScroll({
-  el: document.querySelector('.main'),
-  smooth: true
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  var toggleButton = document.getElementById('toggleButton');
+  var memberDiv = document.getElementById('member');
+  var imageDiv = document.querySelector('.image');
+  var mainMembersDiv = document.querySelector('.main-members');
+
+  if (toggleButton && memberDiv) {
+    toggleButton.addEventListener('click', function() {
+      toggleContent(memberDiv, imageDiv, toggleButton, mainMembersDiv);
+    });
+  }
 });
+
+function toggleContent(memberDiv, imageDiv, toggleButton, mainMembersDiv) {
+  if (memberDiv && toggleButton && mainMembersDiv) {
+    var isVisible = memberDiv.classList.toggle('visible');
+    memberDiv.classList.toggle('hidden');
+    imageDiv.classList.toggle('hidden');
+    console.log(isVisible)
+    // Change button text based on visibility of element
+    toggleButton.textContent = isVisible ? 'Less' : 'More';
+
+    // Scroll to the main-members div if it's being shown
+    if (isVisible) {
+      console.log("hello")
+    } else {
+      // Scroll to the top of the page if hiding the main member div
+      mainMembersDiv.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+  }
+}
